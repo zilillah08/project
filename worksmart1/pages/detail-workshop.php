db241d5f61a1def1f1325a1978ebd6f3f3a49e23
zilillah08 <zilillahlailatul@gmail.com>
2024-11-28 08:59:30 +0700

    menghapus pembayaran credit dan e wallet

--- worksmart1/pages/detail-workshop.php
+++ worksmart1/pages/detail-workshop.php
@@ -249,14 +249,14 @@ $role = $_SESSION['role'];
                                   <label class="form-check-label ms-2" for="transfer">
                                     <i class="bi bi-bank text-primary"></i> Transfer Bank
                                   </label>
-                                <!-- </div>
-                                <div class="payment-option mb-3 p-3 border rounded-3 hover-shadow">
+                                 </div>
+                               <!-- <div class="payment-option mb-3 p-3 border rounded-3 hover-shadow">
                                   <input class="form-check-input" type="radio" name="paymentMethod" id="ewallet" value="ewallet" disabled>
                                   <label class="form-check-label ms-2" for="ewallet">
                                     <i class="bi bi-wallet2 text-success"></i> E-Wallet
                                   </label>
-                                </div> -->
-                                <!-- <div class="payment-option mb-3 p-3 border rounded-3 hover-shadow">
+                                </div> 
+                                 <div class="payment-option mb-3 p-3 border rounded-3 hover-shadow">
                                   <input class="form-check-input" type="radio" name="paymentMethod" id="creditCard" value="creditCard" disabled>
                                   <label class="form-check-label ms-2" for="creditCard">
                                     <i class="bi bi-credit-card text-danger"></i> Kartu Kredit
