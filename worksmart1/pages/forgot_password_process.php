d2454f8869e87802945ce20dc91103255f19e33a
MufridaFaraDiani27 <e31231226@student.polije.ac.id>
2024-12-09 22:13:32 +0700

    Update forgot_password_process.php

--- worksmart1/pages/forgot_password_process.php
+++ worksmart1/pages/forgot_password_process.php
@@ -24,13 +24,13 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                        // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                    // Enable SMTP authentication
-    $mail->Username   = 'pmaya0338@gmail.com';                   // SMTP username
-    $mail->Password   = 'skjp iigu rjbm fqnn';                   // SMTP password
+    $mail->Username   = 'worksmartwmk@gmail.com';                   // SMTP username
+    $mail->Password   = 'maab qxdb djry toks';                   // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;          // Enable TLS encryption; PHPMailer::ENCRYPTION_SMTPS encouraged
     $mail->Port       = 587;                                     // TCP port to connect to, use 465 for PHPMailer::ENCRYPTION_SMTPS above
 
     //Recipients
-    $mail->setFrom('pmaya0338@gmail.com', 'WorkSmart');
+    $mail->setFrom('worksmartwmk@gmail.com', 'WorkSmart');
     $mail->addAddress($email);     // Add a recipient
 
     $code = substr(str_shuffle('1234567890QWERTYUIOPASDFGHJKLZXCVBNM'), 0, 10);
