3d2e92bcb0083d07e8b8f75f4158041c7315260f
MufridaFaraDiani27 <e31231226@student.polije.ac.id>
2024-12-12 02:22:36 +0700

    Update forgot_password_process.php

--- worksmart1/pages/forgot_password_process.php
+++ worksmart1/pages/forgot_password_process.php
@@ -1,111 +1,73 @@
-
-
-
 <?php
 if (isset($_POST['reset'])) {
     $email = $_POST['email'];
 } else {
-    exit();
+    exit('No reset request received.');
 }
 
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require 'mail/Exception.php';
 require 'mail/PHPMailer.php';
 require 'mail/SMTP.php';
 
-// Instantiation and passing true enables exceptions
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                        // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                    // Enable SMTP authentication
-    $mail->Username   = 'worksmartwmk@gmail.com';                   // SMTP username
-    $mail->Password   = 'maab qxdb djry toks';                   // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;          // Enable TLS encryption; PHPMailer::ENCRYPTION_SMTPS encouraged
-    $mail->Port       = 587;                                     // TCP port to connect to, use 465 for PHPMailer::ENCRYPTION_SMTPS above
+    // Konfigurasi SMTP
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'worksmartwmk@gmail.com';
+    $mail->Password   = 'maab qxdb djry toks';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
 
-    //Recipients
+    // Penerima email
     $mail->setFrom('worksmartwmk@gmail.com', 'WorkSmart');
-    $mail->addAddress($email);     // Add a recipient
+    $mail->addAddress($email);
 
+    // Generate kode reset password
     $code = substr(str_shuffle('1234567890QWERTYUIOPASDFGHJKLZXCVBNM'), 0, 10);
 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
+    // Konten email
+    $mail->isHTML(true);
     $mail->Subject = 'Password Reset';
-    $mail->Body = 'To reset your password click <a href="http://localhost/project/worksmart1/pages/change_password.php?email=' . ($email) . '">here</a>. </br>Reset your password in a day.';
+    $mail->Body = 'To reset your password, click <a href="http://localhost/project/worksmart1/pages/change_password.php?email=' . urlencode($email) . '&code=' . $code . '">here</a>. </br>Reset your password within a day.';
 
-
+    // Koneksi ke database
     $conn = new mysqli('localhost', 'root', '', 'worksmart1');
 
     if ($conn->connect_error) {
         die('Could not connect to the database.');
     }
 
+    // Debug: Periksa struktur tabel
+    $result = $conn->query("SHOW COLUMNS FROM users");
+    while ($row = $result->fetch_assoc()) {
+        echo $row['Field'] . '<br>'; // Menampilkan nama kolom untuk memastikan kolom 'code' ada
+    }
+
+    // Verifikasi email
     $verifyQuery = $conn->query("SELECT * FROM users WHERE email = '$email'");
-
-    if ($verifyQuery->num_rows) {
+    if ($verifyQuery->num_rows > 0) {
+        // Update kode reset password
         $codeQuery = $conn->query("UPDATE users SET code = '$code' WHERE email = '$email'");
+        if (!$codeQuery) {
+            die('Query failed: ' . $conn->error);
+        }
 
-
-        // Send email
+        // Kirim email
         $mail->send();
-
-        // Display success message with JavaScript alert
         echo '<script>alert("Message has been sent, check your email");</script>';
+    } else {
+        echo '<script>alert("Email not registered.");</script>';
     }
 
     $conn->close();
 
 } catch (Exception $e) {
-    // Display error message with JavaScript alert
     echo '<script>alert("Message could not be sent. Mailer Error: ' . $mail->ErrorInfo . '");</script>';
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!-- Document Title -->
-    <title class="brand-color">Forgot Password</title>
-<!-- Favicons -->
-<link href="../pages/assets/img/logo-worksmart.png" rel="icon">
-    <!-- External CSS Links -->
-    <link
-      crossorigin="anonymous"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-      rel="stylesheet"
-    />
-    <link
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-      rel="stylesheet"
-    />
-    <link href="pages/assets/css/brand.css" rel="stylesheet" />
-
-    <!-- Custom Styles -->
-    <style>
-      body {
-        background-color: #02396f;
-        font-family: "Arial", sans-serif;
-      }
-    </style>
-  </head>
-
-  <body>
-    <div id="loading-overlay" style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(255,255,255,0.8); z-index: 9999;">
-      <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-        <div class="spinner-border text-primary" role="status">
-          <span class="visually-hidden">Loading...</span>
-        </div>
-      </div>
-    </div>
-    <!-- Your page content goes here -->
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
-</html>
